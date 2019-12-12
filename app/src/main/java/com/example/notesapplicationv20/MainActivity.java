34f26880f6e11e479aed9a94bc1339df7f037c92
TimB1990 <t.boons@live.nl>
2019-12-12 20:45:02 +0100

    working selection but no intent to other activity notice todo: On item click no delete!

--- app/src/main/java/com/example/notesapplicationv20/MainActivity.java
+++ app/src/main/java/com/example/notesapplicationv20/MainActivity.java
@@ -3,55 +3,46 @@ package com.example.notesapplicationv20;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.notesapplicationv20.adapter.NotesAdapter;
 import com.example.notesapplicationv20.database.ListedNote;
-import com.example.notesapplicationv20.database.SingleNote;
 import com.example.notesapplicationv20.viewmodel.NotesViewModel;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NotesAdapter.ViewHolder.ClickListener{
 
    private static final int NEW_NOTE_REQUEST_CODE = 1;
+   private static final String TAG = MainActivity.class.getSimpleName();
 
    private RecyclerView rvNotes;
    private NotesAdapter adapter;
+   private ActionModeCallback actionModeCallback;
+   private ActionMode actionMode;
    private NotesViewModel notesVm;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
-
+      actionModeCallback = new ActionModeCallback();
       rvNotes = findViewById(R.id.notes_recycler_view);
-
-      adapter = new NotesAdapter(this);
-      adapter.setOnItemClickListener(new NotesAdapter.ClickListener() {
-         @Override
-         public void onItemClick(int position, View v) {
-            String text = "onItemClick: " + position;
-            Toast.makeText(getApplicationContext(),text, Toast.LENGTH_SHORT).show();
-         }
-
-         @Override
-         public void onItemLongClick(int position, View v) {
-            String text = "onItemLongClick: " + position;
-            Toast.makeText(getApplication(), text, Toast.LENGTH_SHORT).show();
-         }
-      });
-
+      adapter = new NotesAdapter(getApplicationContext(),this);
       notesVm = new ViewModelProvider(this).get(NotesViewModel.class);
+
       notesVm.getListedNotes().observe(this, new Observer<List<ListedNote>>(){
          @Override
          public void onChanged(List<ListedNote> listedNotes) {
@@ -60,7 +51,9 @@ public class MainActivity extends AppCompatActivity {
       });
 
       rvNotes.setAdapter(adapter);
+      rvNotes.setItemAnimator(new DefaultItemAnimator());
       rvNotes.setLayoutManager(new LinearLayoutManager(this));
+      //rvNotes.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
 
       // create button logic
       Button createButton = findViewById(R.id.create_note_button);
@@ -68,6 +61,38 @@ public class MainActivity extends AppCompatActivity {
          Intent intent = new Intent(MainActivity.this, WriteNoteActivity.class);
          startActivityForResult(intent, NEW_NOTE_REQUEST_CODE);
       });
+   }
+
+   @Override
+   public void onItemClicked(int position) {
+      if(actionMode != null){
+         toggleSelection(position);
+      }
+      else{
+         adapter.removeItem(position);
+      }
+   }
+
+   @Override
+   public boolean onItemLongClicked(int position) {
+      if(actionMode == null){
+         actionMode = startActionMode(actionModeCallback);
+      }
+
+      toggleSelection(position);
+      return true;
+   }
+
+   private void toggleSelection(int position){
+      adapter.toggleSelection(position);
+      int count = adapter.getSelectedItemCount();
+      if(count == 0){
+         actionMode.finish();
+      }
+      else{
+         actionMode.setTitle(String.valueOf(count));
+         actionMode.invalidate();
+      }
    }
 
    public void onActivityResult(int requestCode, int resultCode, Intent data){
@@ -79,6 +104,41 @@ public class MainActivity extends AppCompatActivity {
                          WriteNoteActivity.EXTRA_REPLY
                  ),
                  Toast.LENGTH_SHORT).show();
+      }
+   }
+
+   private class ActionModeCallback implements ActionMode.Callback{
+      private final String TAG = ActionModeCallback.class.getSimpleName();
+
+      @Override
+      public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+         mode.getMenuInflater().inflate(R.menu.selected_menu, menu);
+         return true;
+      }
+
+      @Override
+      public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+         return false;
+      }
+
+      @Override
+      public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+         switch(item.getItemId()){
+            case R.id.menu_remove:
+               adapter.removeItems(adapter.getSelectedItems());
+               // TODO actual remove from database implementation
+               Log.d(TAG, "menu_remove");
+               mode.finish();
+               return true;
+            default:
+               return false;
+         }
+      }
+
+      @Override
+      public void onDestroyActionMode(ActionMode mode) {
+         adapter.clearSelection();
+         actionMode = null;
       }
    }
 }
--- app/src/main/java/com/example/notesapplicationv20/adapter/NotesAdapter.java
+++ app/src/main/java/com/example/notesapplicationv20/adapter/NotesAdapter.java
@@ -4,47 +4,49 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import com.example.notesapplicationv20.R;
 import com.example.notesapplicationv20.database.ListedNote;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder>{
+public class NotesAdapter extends SelectableAdapter<NotesAdapter.ViewHolder>{
 
+   private Context context;
 
-   private Context mContext;
-   private static ClickListener clickListener;
+   private ViewHolder.ClickListener cl;
 
    private final LayoutInflater mInflater;
    private List<ListedNote> mListedNotes;
 
-   MyCallback myCallback;
 
-   public interface MyCallback{
-      void listenerMethod(String textViewValue);
-   }
-
-   public NotesAdapter(Context context) {
-      this.mContext = context;
-      mInflater = LayoutInflater.from(context);
+   public NotesAdapter(Context context, ViewHolder.ClickListener cl) {
+      super();
+      this.cl = cl;
+      this.context = context;
+      this.mInflater = LayoutInflater.from(context);
    }
 
    @NonNull
    @Override
    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
+      // final int layout = viewType == TYPE_INACTIVE ? R.layout.note_item : R.layout.note_item_active;
+
       View itemView = mInflater.inflate(R.layout.note_item, parent, false);
-      return new ViewHolder(itemView);
+      return new ViewHolder(itemView, cl);
    }
 
    @Override
    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-      ListedNote lNote = mListedNotes.get(position);
+      final ListedNote lNote = mListedNotes.get(position);
+
       holder.id.setText(Integer.toString(lNote.getId()));
       holder.subject.setText(lNote.getSubject());
       holder.title.setText(lNote.getTitle());
@@ -53,6 +55,20 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder>{
       holder.createdAt.setText(sdf.format(lNote.getCreatedAt()));
       holder.lastUpdate.setText(sdf.format(lNote.getLastUpdate()));
 
+      // span the item if active, final can only be assigned once
+      final ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
+      if(lp instanceof StaggeredGridLayoutManager.LayoutParams){
+         StaggeredGridLayoutManager.LayoutParams sglp = (StaggeredGridLayoutManager.LayoutParams) lp;
+         sglp.setFullSpan(lNote.isSelected());
+         holder.itemView.setLayoutParams(sglp);
+      }
+
+      // highlight the item if it is selected
+      // holder.itemLayout.setVisibility(isSelected(position) ? View.VISIBLE : View.INVISIBLE);
+      holder.cardViewLayout.setBackgroundColor(
+              isSelected(position) ? context.getResources().getColor(R.color.selected_overlay) :
+                      context.getResources().getColor(R.color.item_standard)
+      );
    }
 
    // set listed notes
@@ -70,45 +86,100 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder>{
       }
    }
 
-   public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener  {
+   public void removeItem(int position){
+      mListedNotes.remove(position);
+      notifyItemRemoved(position);
+   }
 
-      public TextView id, subject, title, description, createdAt, lastUpdate;
-      public LinearLayout itemLayout;
+   public void removeItems(List<Integer> positions){
+      Collections.sort(positions, new Comparator<Integer>(){
+         @Override
+         public int compare(Integer lhs, Integer rhs) {
+            return rhs - lhs;
+         }
+      });
+
+      // split the list in ranges
+      while (!positions.isEmpty()) {
+         if (positions.size() == 1) {
+            removeItem(positions.get(0));
+            positions.remove(0);
+         } else {
+            int count = 1;
+            while (positions.size() > count && positions.get(count).equals(positions.get(count - 1) - 1)) {
+               ++count;
+            }
+
+            if (count == 1) {
+               removeItem(positions.get(0));
+            } else {
+               removeRange(positions.get(count - 1), count);
+            }
+
+            for (int i = 0; i < count; ++i) {
+               positions.remove(0);
+            }
+         }
+      }
+   }
+
+   private void removeRange(int positionStart, int itemCount){
+      for(int i = 0; i < itemCount; ++i){
+         mListedNotes.remove(positionStart);
+      }
+
+      notifyItemRangeRemoved(positionStart,itemCount);
+   }
 
 
-      public ViewHolder(View itemView) {
+
+   public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
+
+      private static final String TAG = ViewHolder.class.getSimpleName();
+      TextView id, subject, title, description, createdAt, lastUpdate;
+      View cardViewLayout;
+
+      // part of listener
+      private ClickListener listener;
+
+      public ViewHolder(View itemView, ClickListener listener) {
          super(itemView);
 
-         itemView.setOnClickListener(this);
-         itemView.setOnLongClickListener(this);
-
-         itemLayout = itemView.findViewById(R.id.item_layout);
          id = itemView.findViewById(R.id.note_id);
          subject = itemView.findViewById(R.id.note_subject_text);
          title = itemView.findViewById(R.id.note_title_text);
          description = itemView.findViewById(R.id.note_description_text);
          createdAt = itemView.findViewById(R.id.note_created_text);
          lastUpdate = itemView.findViewById(R.id.note_updated_text);
+         cardViewLayout = itemView.findViewById(R.id.card_view_layout);
+
+         this.listener = listener;
+
+         itemView.setOnClickListener(this);
+         itemView.setOnLongClickListener(this);
       }
 
+      // logic for exposing listener passed to adapter
       @Override
       public void onClick(View v) {
-         clickListener.onItemClick(getAdapterPosition(), v);
+         if(listener != null){
+            listener.onItemClicked(getLayoutPosition());
+         }
       }
 
       @Override
       public boolean onLongClick(View v) {
-         clickListener.onItemLongClick(getAdapterPosition(),v);
+         if(listener != null){
+            return listener.onItemLongClicked(getLayoutPosition());
+         }
+
          return true;
       }
-   }
 
-   public void setOnItemClickListener(ClickListener clickListener){
-      NotesAdapter.clickListener = clickListener;
-   }
+      public interface ClickListener{
+         void onItemClicked(int position);
+         boolean onItemLongClicked(int position);
+      }
 
-   public interface ClickListener{
-      void onItemClick(int position, View v);
-      void onItemLongClick(int position, View v);
    }
 }
